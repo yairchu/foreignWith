1c9dd1a0df95051b033aafe026cd62e3c2b2d84f
Yair Chuchem <yairchu@yair-chuchems-macbook.local>
2009-06-29 15:18:11 +0300

    added allocaBytes, refactored

--- src/Foreign/With.hs
+++ src/Foreign/With.hs
@@ -1,13 +1,13 @@
 {-# OPTIONS -O2 -Wall #-}
 
 module Foreign.With (
-  alloca, allocaArray, withForeignPtr
+  alloca, allocaArray, allocaBytes, withForeignPtr
   ) where
 
 import Control.Monad.Trans (MonadIO(..))
 import Foreign.ForeignPtr (
   ForeignPtr, newForeignPtr, touchForeignPtr, unsafeForeignPtrToPtr)
-import Foreign.Marshal.Alloc (finalizerFree, malloc)
+import Foreign.Marshal.Alloc (finalizerFree, malloc, mallocBytes)
 import Foreign.Marshal.Array (mallocArray)
 import Foreign.Ptr (Ptr)
 import Foreign.Storable (Storable)
@@ -18,16 +18,18 @@ withForeignPtr fptr func = do
   liftIO $ touchForeignPtr fptr
   return r
 
-alloca :: (MonadIO m, Storable a) => (Ptr a -> m b) -> m b
-alloca func = do
-  fptr <- liftIO $ newForeignPtr finalizerFree =<< malloc
+alloca' :: MonadIO m => IO (Ptr a) -> (Ptr a -> m b) -> m b
+alloca' mallocFunc func = do
+  fptr <- liftIO $ newForeignPtr finalizerFree =<< mallocFunc
   withForeignPtr fptr func
 
+alloca :: (MonadIO m, Storable a) => (Ptr a -> m b) -> m b
+alloca = alloca' malloc
+
 allocaArray ::
-  (MonadIO m, Storable a, Integral i) =>
-  i -> (Ptr a -> m b) -> m b
-allocaArray size func = do
-  fptr <- liftIO $ newForeignPtr finalizerFree =<<
-    mallocArray (fromIntegral size)
-  withForeignPtr fptr func
+  (MonadIO m, Storable a, Integral i) => i -> (Ptr a -> m b) -> m b
+allocaArray = alloca' . mallocArray . fromIntegral
+
+allocaBytes :: (MonadIO m, Integral i) => i -> (Ptr a -> m b) -> m b
+allocaBytes = alloca' . mallocBytes . fromIntegral
 
